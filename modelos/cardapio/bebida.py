474e669ff2d211a8208cd016e1d7cb0aaacd58ff
taiane94sb <taiane19sb94@gmail.com>
2024-04-03 21:05:27 -0300

    herança

--- modelos/cardapio/bebida.py
+++ modelos/cardapio/bebida.py
@@ -1,4 +1,8 @@
-class Bebida:
-    def __init__(self, nome, preco, descricao, tamanho):
-        ...
+from modelos.cardapio.item_cardapio import ItemCardapio
+
+
+class Bebida(ItemCardapio):
+    def __init__(self, nome, preco, tamanho):
+        super().__init__(nome, preco)
+        self.tamanho = tamanho
     
--- modelos/cardapio/prato.py
+++ modelos/cardapio/prato.py
@@ -1,4 +1,8 @@
-class Prato:
+from modelos.cardapio.item_cardapio import ItemCardapio
+
+
+class Prato(ItemCardapio):
     def __init__(self, nome, preco, descricao):
-        ...
+        super().__init__ (nome, preco)
+        self.descricao = descricao
     
