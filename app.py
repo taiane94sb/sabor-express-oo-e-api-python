231e7a81dd2bc42f7ff743fe3c672c794246b1cf
taiane94sb <taiane19sb94@gmail.com>
2024-04-03 21:13:07 -0300

    métodos para adicionar itens

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ restaurante_praca = Restaurante('praça', 'Gourmet')
 prato_paozinho = Prato('Paozinho', 2.00, 'O melhor pão da cidade')
 bebida_suco = Bebida ('Suco de Melancia', 5.0, 'grande')
 
+restaurante_praca.adicionar_no_cardapio(prato_paozinho)
+restaurante_praca.adicionar_no_cardapio(bebida_suco)
+
 def main():
     print(prato_paozinho)
     print(bebida_suco)
