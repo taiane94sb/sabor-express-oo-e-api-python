fe1b764c7233431a359381773e1b5859c73fb287
taiane94sb <taiane19sb94@gmail.com>
2024-04-04 21:31:51 -0300

    consultando um endpoint

--- main.py
+++ main.py
@@ -7,10 +7,17 @@ app = FastAPI()
 
 @app.get('/api/hello')
 def hello_world():
+    '''
+    Endpoint que exibe uma mensagem de Hello World
+    '''
     return {'Hello': 'World'}
 
 @app.get('/api/restaurantes/')
 def get_restaurantes(restaurante: str = Query(None)):
+    '''
+    Endpoint para ver os cardápios dos restaurantes  
+        
+    '''
     url = 'https://guilhermeonrails.github.io/api-restaurantes/restaurantes.json'
     response = requests.get(url)
 
